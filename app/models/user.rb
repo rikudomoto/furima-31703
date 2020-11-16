a690aa4391aec2c61d83dfbde9d3b27e550c16cb
rikudomoto <73264978+rikudomoto@users.noreply.github.com>
2020-11-16 13:54:06 +0900

    トップページから新規登録＆ログイン遷移

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,             presence: true
+  validates :family_name,          presence: true
+  validates :first_name,           presence: true
+  validates :family_name_katakana, presence: true
+  validates :first_name_katakana,  presence: true
+  validates :birthday,             presence: true
+  
+
 end
