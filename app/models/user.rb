e1b4ec4606a76fc74f4117cc1da24003dd82aa38
rikudomoto <73264978+rikudomoto@users.noreply.github.com>
2020-11-16 16:53:05 +0900

    パスワード半角英数字制約

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,11 @@ class User < ApplicationRecord
   validates :first_name_katakana,  presence: true
   validates :birthday,             presence: true
   
-
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of  :password,   with: PASSWORD_REGEX, message: "Password Include both letters and numbers"
+  
+  # with_options presence: true do
+  #   validates :password,   format: { with: /\A[a-zA-Z0-9]+\z/i,message: "Password Include both letters and numbers"}
+  #   validates :password_confirmation, format: { with: /\A[a-zA-Z0-9]+\z/i,message: "Password Include both letters and numbers"}
+  # end
 end
