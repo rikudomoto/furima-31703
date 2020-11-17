206dd663acc3a2ef3f2e3991a229b599f0c0191e
rikudomoto <73264978+rikudomoto@users.noreply.github.com>
2020-11-17 18:32:23 +0900

    商品画像のActive Storage導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name,:category_id,:status_id,:delivery_fee_id,:shipping_area_id,:says_to_ship_id,:price,:explanation).merge(user_id: current_user.id)
+    params.require(:item).permit(:name,:category_id,:status_id,:delivery_fee_id,:shipping_area_id,:says_to_ship_id,:price,:explanation,:image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category, :status, :delivery_fee, :shipping_area, :says_to_ship
+  has_one_attached :image
   belongs_to :user
   has_one    :buy
 
