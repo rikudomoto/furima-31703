46d7f268903d7781bafc50da4d7f30d25dbe0d2a
rikudomoto <73264978+rikudomoto@users.noreply.github.com>
2020-11-18 17:05:49 +0900

    ダブルクォーテーション修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
   factory :item do
     name                  { '商品名' }
-    category_id           { '2' }
-    status_id             { '2' }
-    delivery_fee_id       { '2' }
-    shipping_area_id      { '2' }
-    says_to_ship_id       { '2' }
-    price                 { '500' }
+    category_id           { 2 }
+    status_id             { 2 }
+    delivery_fee_id       { 2 }
+    shipping_area_id      { 2 }
+    says_to_ship_id       { 2 }
+    price                 { 500 }
     explanation           { Faker::Lorem.sentence }
     association :user
     after(:build) do |item|
