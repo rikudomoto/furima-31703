9520b2aa2b9dcedc74cb87117c998e17a746c1f1
rikudomoto <73264978+rikudomoto@users.noreply.github.com>
2020-11-20 15:04:27 +0900

    削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,12 @@ class ItemsController < ApplicationController
 
   def show
   end
+  
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end
 
   def edit
     unless @item.user_id == current_user.id
