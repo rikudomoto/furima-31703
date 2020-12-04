1de3cdd0c905ea26d8ab64d1d520d971591e9a78
rikudomoto <73264978+rikudomoto@users.noreply.github.com>
2020-12-04 16:08:03 +0900

    test

---
+++ config/webpacker_env.rb
@@ -0,0 +1,2 @@
+Webpacker::Compiler.env["PAYJP_PUBLIC_KEY"] = Rails.application.credentials.payjp[:public_key]
+Webpacker::Compiler.env["PAYJP_SECRET_KEY"] = Rails.application.credentials.payjp[:secret_key]
